97c52a72fa72b633cbcd62004f8449165bd7ff0f
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-08-26 15:58:12 +0500

    changed

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 handler404 = 'tyres.views.custom_404'
 
 # Static files are now served by whitenoise middleware
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT, }, ), ]
 
